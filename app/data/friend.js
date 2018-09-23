b9fa3a39e99ee80b4e5b989983bcee25af5732eb
Nero_Sen <niravsen11@gmail.com>
2018-09-23 19:51:47 -0400

    apps folder

---
+++ app/data/friend.js
@@ -0,0 +1,21 @@
+
+var friendList =[
+{
+    "name":"Nirav Sen",
+    "photo":"https://avatars2.githubusercontent.com/u/38270475?s=400",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+ }
+]
+
+module.exports = friendList;
